9f7d093137a6efc8f2a0b639be6f15af2856f986
unknown <tiago_rimbaud@hotmail.com>
2020-02-23 00:37:34 +0100

    add doc

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 var express = require ('express');
 
 var app = express();
+var PouchDB = require('pouchdb');
+var sensor = require("node-dht-sensor");
 
 //console.log('hello wordl');
 
-//var sensor = require("node-dht-sensor");
+
 
 
 setInterval(function(){logIt();}, 5000);
 
-
+setInterval(function(){insertData("airSensor");}, 60000);
 
 
 function logIt (){
-	//sensor.read(11, 2, function(err, temperature, humidity){
+	sensor.read(11, 2, function(err, temperature, humidity){
 
 		if(!err){
-			//console.log('temp: ' + temperature + ', humidity: ' + humidity + '%');
+			console.log('temp: ' + temperature + ', humidity: ' + humidity + '%');
 		}
-	//});
+	});
 }
 
 
@@ -26,7 +28,31 @@ app.get('/MyState', function(req, res){
 
 });
 
+function insertData(sensorType){
 
+	sensor.read(11, 2, function(err, temperature, humidity){
+
+		try{
+			
+			var today = new Date();
+			var time = today.getFullYear()+'_'+(today.getMonth()+1) + "_" + today.getMinutes();
+			var db = new PouchDB('http://127.0.0.1:5984/sensor_data');
+			var doc = {
+			"_id": time, 
+			"type": sensorType,
+			"temp": temperature,
+			"humidity": humidity,
+			"jour": today.getDay(),
+			"minute": today.getMinutes()
+			}
+
+			db.put(doc);
+		}catch(err){
+			throw err;
+		}
+	});
+	
+}
 
 
 
